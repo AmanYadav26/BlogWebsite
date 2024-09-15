6a7bd2c6fa4f56b92105be051d4ad8db5f1e2c37
Aman Yadav <99034773+AmanYadav26@users.noreply.github.com>
2024-09-15 18:58:17 +0530

    Update createblog.js

--- my-blog/src/createblog.js
+++ my-blog/src/createblog.js
@@ -8,7 +8,7 @@ function createPost(history) {
     };
 
     var xhr = new XMLHttpRequest();
-    xhr.open('POST', 'http://localhost:3000/posts', true);
+    xhr.open('POST', 'https://blogwebsite-backend-yg9k.onrender.com/posts', true);
     xhr.setRequestHeader('Content-Type', 'application/json');
     xhr.setRequestHeader('Authorization', 'Bearer ' + localStorage.getItem('jwtToken'));
     xhr.onreadystatechange = function () {
